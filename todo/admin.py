6501056e35bff90062db185d6d1dc0996c55b82d
Rhogiel Andoy <theglitch0710@gmail.com>
2021-12-31 17:36:56 +0800

    Design Todo Model

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Todo
 
-admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_created',)
+
+
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=300)
-    desc = models.TextField()
+    memo = models.TextField(blank=True)
+    important = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
-    date_completed = models.DateTimeField(auto_now=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    createdby = models.ForeignKey(User, on_delete=models.CASCADE, default=False)
 
     def __str__(self):
         return self.title
-
